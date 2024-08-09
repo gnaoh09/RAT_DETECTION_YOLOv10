19edc33a9b6d6c849a9a97582ce2e88be513e59f
gnaoh09 <92804109+gnaoh09@users.noreply.github.com>
2024-08-09 11:15:12 +0700

    Add files via upload

---
+++ test_cam.py
@@ -0,0 +1,52 @@
+import cv2
+from picamera2 import Picamera2
+
+from ultralytics import YOLO
+
+# Initialize the Picamera2
+picam2 = Picamera2()
+picam2.preview_configuration.main.size = (1280, 720)
+picam2.preview_configuration.main.format = "RGB888"
+picam2.preview_configuration.align()
+picam2.configure("preview")
+picam2.start()
+
+# Load the YOLOv8 model
+model = YOLO("yolov8n.pt")
+
+while True:
+    # Capture frame-by-frame
+    frame = picam2.capture_array()
+
+    # Run YOLOv8 inference on the frame
+    results = model(frame)
+
+    # Visualize the results on the frame
+    annotated_frame = results[0].plot()
+
+    # Display the resulting frame
+    cv2.imshow("Camera", annotated_frame)
+
+    # Break the loop if 'q' is pressed
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# Release resources and close windows
+cv2.destroyAllWindows()
+
+
+
+
+""" from ultralytics import YOLO
+
+# Load a YOLOv8n PyTorch model
+model = YOLO("yolov8n.pt")
+
+# Export the model to NCNN format
+model.export(format="ncnn")  # creates 'yolov8n_ncnn_model'
+
+# Load the exported NCNN model
+ncnn_model = YOLO("yolov8n_ncnn_model")
+
+# Run inference
+results = ncnn_model("https://ultralytics.com/images/bus.jpg") """
